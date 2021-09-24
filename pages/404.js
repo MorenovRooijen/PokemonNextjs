db8fcbccdb057337368f8f417860da1af6160d32
Moreno van Rooijen <69149271+MorenovRooijen@users.noreply.github.com>
2021-09-24 16:37:30 +0200

    feat: add 404

---
+++ pages/404.js
@@ -0,0 +1,15 @@
+import ComponentName from "components/ComponentName";
+import PageContainer from "components/PageLayout/PageContainer";
+const Page404 = () => {
+  return (
+    <>
+      <PageContainer>
+        <h1>404</h1>
+        <h4>Geen pagina gevonden.</h4>
+        <ComponentName />
+      </PageContainer>
+    </>
+  );
+};
+
+export default Page404;
