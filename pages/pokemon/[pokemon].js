dc3e5559affcc9e94d57f6b4c6225fe492507bb6
Moreno van Rooijen <69149271+MorenovRooijen@users.noreply.github.com>
2021-11-24 10:24:12 +0100

    fix: remove page

--- pages/pokemon/[pokemon].js
+++ pages/pokemon/[pokemon].js
@@ -1,69 +1,11 @@
 import PageContainer from "components/PageLayout/PageContainer";
-import PokemonImage from "components/PokemonImage";
 import { API } from "hooks/pokemon_api_calls";
-import { useEffect, useState } from "react";
 
 const PokemonPage = ({ pokemon }) => {
-  const [species, setSpecies] = useState(null);
-  const [evolution, setEvolution] = useState(null);
-  useEffect(() => {
-    console.log(pokemon.id);
-    API.species.get(pokemon.id).then((species) => {
-      console.log("species", species);
-      setSpecies(species);
-      API.evolution.get(species.id).then((data) => {
-        console.log("evolution", data);
-        setEvolution(data);
-      });
-    });
-  }, []);
   return (
     <PageContainer>
       <h2>{pokemon?.name}</h2>
-      <PokemonImage
-        src={pokemon.sprites.other["official-artwork"].front_default}
-        alt=""
-      />
-      <div className="container">
-        <div className="label">Height</div>
-        <div className="value">{pokemon.height}</div>
-        <div className="label">Weight</div>
-        <div className="value">{pokemon.weight}</div>
-        <div className="label">Types</div>
-        <div className="value">
-          {pokemon.types.map((variation_item) => {
-            console.log(variation_item);
-            return <div className="type">{variation_item.type.name}</div>;
-          })}
-        </div>
-        {species && (
-          <>
-            <div className="label">Area</div>
-            <div className="value">{species?.habitat?.name}</div>
-          </>
-        )}
-        {evolution?.chain && evolution?.chain?.evolves_to && (
-          <>
-            <div className="label">Evolves to</div>
-            <div className="evolution">
-              {evolution?.chain?.evolves_to.map((evolution) => {
-                return (
-                  <>
-                    {evolution.species.name}
-                    {evolution?.evolves_to && (
-                      <>
-                        {evolution?.evolves_to.map((evolution2) => {
-                          return "-" + evolution2.species.name;
-                        })}
-                      </>
-                    )}
-                  </>
-                );
-              })}
-            </div>
-          </>
-        )}
-      </div>
+      <pre>{JSON.stringify(pokemon, null, 2)}</pre>
 
       <style jsx>{`
         .container {
